08da210adf4b50bd17e15c62d6927ea635c6d491
Kantonio Brownlee <kantoniosean@yahoo.com>
2023-07-19 00:41:29 -0500

    new factore

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
 import "./style.css"
 
 export default function App() {
-  return "hi"
+  return (
+    <form className="evaluation-standards">
+      <div className="factor">
+        <label htmlFor="new-factor">Add New Factor</label>
+        <input type="text" id="new-factor"></input>
+      </div>
+    </form>
+  )
 }
