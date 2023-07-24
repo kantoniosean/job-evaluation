9915001e4f56db2d755e6f849f076b255581e8bb
Kantonio Brownlee <kantoniosean@yahoo.com>
2023-07-24 13:48:39 -0500

    new form

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,11 @@ import "./style.css"
 
 export default function App() {
   return (
-    <form className="evaluation-standards">
-      <div className="factor">
-        <input type="text" id="new-factor" className="input"></input>
-        <span htmlFor="new-factor" className="newFactor">Add New Factor</span>
-      </div>
+    <form className="new-form">
+        <div className="form-factor">
+          <input type="text" id="new-factor" className="input"></input>
+          <button className="mew-button">Add</button>
+        </div>
     </form>
   )
 }
