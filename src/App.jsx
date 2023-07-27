2fdbd066ca0bae1710f8bafe84e36da67f2f4197
Kantonio Brownlee <kantoniosean@yahoo.com>
2023-07-27 09:17:54 -0500

    new UI

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,33 @@ export default function App() {
   }
   const [numbers, setData] = useState(data)
 
-  var result;
+  const [val1, setVal1] = useState([]);
+
+  const [val2, setVal2] = useState([]);
+
+  const [val3, setVal3] = useState([]);
+
+  const [val4, setVal4] = useState([]);
+
+  const handleAddOne=()=>{
+    const newIn=[...val1, []]
+    setVal1(newIn)
+  }
+
+  const handleAddTwo=()=>{
+    const newIn=[...val2, []]
+    setVal2(newIn)
+  }
+
+  const handleAddThree=()=>{
+    const newIn=[...val3, []]
+    setVal3(newIn)
+  }
+
+  const handleAddFour=()=>{
+    const newIn=[...val4, []]
+    setVal4(newIn)
+  }
 
   function add() {
     numbers.result = parseInt(numbers.number1) + parseInt(numbers.number2);
@@ -25,13 +51,230 @@ export default function App() {
 
   return (
     <>
-    <form className="new-form">
-        <div className="form-factor">
-          <input type="number" id="new-factor" className="input" onChange={handleChange}></input>
-          <input type="number" onChange={handleChange}></input>
-          <button onClick={display} className="mew-button">Add</button>
+      {/*form for first factor*/}
+      <div className="form-factor">
+        <div className="first-factor">
+          <input type="text" id="new-factor" className="input" onChange={handleChange}></input>
         </div>
-    </form>
+        <div className="sub-one">
+            <div>
+              <input className="sub-name" onChange={(e=>handleChange(e,i))}/>
+            </div>
+            <div>
+              <input className="sub-weight" onChange={(e=>handleChange(e,i))}/>
+            </div>
+            <div>
+              <input className="sub-point" onChange={(e=>handleChange(e,i))}/>
+            </div>
+            <div>
+              <input className="sub-des" onChange={(e=>handleChange(e,i))}/>
+            </div>
+          </div>
+          <div className="sub-one">
+            <div>
+              <input className="sub-name" onChange={(e=>handleChange(e,i))}/>
+            </div>
+            <div>
+              <input className="sub-weight" onChange={(e=>handleChange(e,i))}/>
+            </div>
+            <div>
+              <input className="sub-point" onChange={(e=>handleChange(e,i))}/>
+            </div>
+            <div>
+              <input className="sub-des" onChange={(e=>handleChange(e,i))}/>
+            </div>
+          </div>
+          {val1.map((data,i)=>{
+            return(
+              <div className="sub-one">
+                <div>
+                  <input className="sub-name" onChange={(e=>handleChange(e,i))}/>
+                </div>
+                <div>
+                  <input className="sub-weight" onChange={(e=>handleChange(e,i))}/>
+                </div>
+                <div>
+                  <input className="sub-point" onChange={(e=>handleChange(e,i))}/>
+                </div>
+                <div>
+                  <input className="sub-des" onChange={(e=>handleChange(e,i))}/>
+                </div>
+              </div>
+            )
+          })}
+        <div className="new-button">
+          <button onClick={()=>handleAddOne()}>Add</button>
+        </div>
+      </div>
+
+      {/*form for second factor*/}
+      <div className="form-factor">
+        <div className="first-factor">
+          <input type="text" id="new-factor" className="input" onChange={handleChange}></input>
+        </div>
+        <div className="sub-one">
+            <div>
+              <input className="sub-name" onChange={(e=>handleChange(e,i))}/>
+            </div>
+            <div>
+              <input className="sub-weight" onChange={(e=>handleChange(e,i))}/>
+            </div>
+            <div>
+              <input className="sub-point" onChange={(e=>handleChange(e,i))}/>
+            </div>
+            <div>
+              <input className="sub-des" onChange={(e=>handleChange(e,i))}/>
+            </div>
+          </div>
+          <div className="sub-one">
+            <div>
+              <input className="sub-name" onChange={(e=>handleChange(e,i))}/>
+            </div>
+            <div>
+              <input className="sub-weight" onChange={(e=>handleChange(e,i))}/>
+            </div>
+            <div>
+              <input className="sub-point" onChange={(e=>handleChange(e,i))}/>
+            </div>
+            <div>
+              <input className="sub-des" onChange={(e=>handleChange(e,i))}/>
+            </div>
+          </div>
+          {val2.map((data,i)=>{
+            return(
+              <div className="sub-one">
+                <div>
+                  <input className="sub-name" onChange={(e=>handleChange(e,i))}/>
+                </div>
+                <div>
+                  <input className="sub-weight" onChange={(e=>handleChange(e,i))}/>
+                </div>
+                <div>
+                  <input className="sub-point" onChange={(e=>handleChange(e,i))}/>
+                </div>
+                <div>
+                  <input className="sub-des" onChange={(e=>handleChange(e,i))}/>
+                </div>
+              </div>
+            )
+          })}
+        <div className="new-button">
+          <button onClick={()=>handleAddTwo()}>Add</button>
+        </div>
+      </div>
+      
+      {/*form for third factor*/}
+      <div className="form-factor">
+        <div className="first-factor">
+          <input type="text" id="new-factor" className="input" onChange={handleChange}></input>
+        </div>
+        <div className="sub-one">
+            <div>
+              <input className="sub-name" onChange={(e=>handleChange(e,i))}/>
+            </div>
+            <div>
+              <input className="sub-weight" onChange={(e=>handleChange(e,i))}/>
+            </div>
+            <div>
+              <input className="sub-point" onChange={(e=>handleChange(e,i))}/>
+            </div>
+            <div>
+              <input className="sub-des" onChange={(e=>handleChange(e,i))}/>
+            </div>
+          </div>
+          <div className="sub-one">
+            <div>
+              <input className="sub-name" onChange={(e=>handleChange(e,i))}/>
+            </div>
+            <div>
+              <input className="sub-weight" onChange={(e=>handleChange(e,i))}/>
+            </div>
+            <div>
+              <input className="sub-point" onChange={(e=>handleChange(e,i))}/>
+            </div>
+            <div>
+              <input className="sub-des" onChange={(e=>handleChange(e,i))}/>
+            </div>
+          </div>
+          {val3.map((data,i)=>{
+            return(
+              <div className="sub-one">
+                <div>
+                  <input className="sub-name" onChange={(e=>handleChange(e,i))}/>
+                </div>
+                <div>
+                  <input className="sub-weight" onChange={(e=>handleChange(e,i))}/>
+                </div>
+                <div>
+                  <input className="sub-point" onChange={(e=>handleChange(e,i))}/>
+                </div>
+                <div>
+                  <input className="sub-des" onChange={(e=>handleChange(e,i))}/>
+                </div>
+              </div>
+            )
+          })}
+        <div className="new-button">
+          <button onClick={()=>handleAddThree()}>Add</button>
+        </div>
+      </div>
+      
+      {/*form for fourth factor*/}
+      <div className="form-factor">
+        <div className="first-factor">
+          <input type="text" id="new-factor" className="input" onChange={handleChange}></input>
+        </div>
+        <div className="sub-one">
+            <div>
+              <input className="sub-name" onChange={(e=>handleChange(e,i))}/>
+            </div>
+            <div>
+              <input className="sub-weight" onChange={(e=>handleChange(e,i))}/>
+            </div>
+            <div>
+              <input className="sub-point" onChange={(e=>handleChange(e,i))}/>
+            </div>
+            <div>
+              <input className="sub-des" onChange={(e=>handleChange(e,i))}/>
+            </div>
+          </div>
+          <div className="sub-one">
+            <div>
+              <input className="sub-name" onChange={(e=>handleChange(e,i))}/>
+            </div>
+            <div>
+              <input className="sub-weight" onChange={(e=>handleChange(e,i))}/>
+            </div>
+            <div>
+              <input className="sub-point" onChange={(e=>handleChange(e,i))}/>
+            </div>
+            <div>
+              <input className="sub-des" onChange={(e=>handleChange(e,i))}/>
+            </div>
+          </div>
+          {val4.map((data,i)=>{
+            return(
+              <div className="sub-one">
+                <div>
+                  <input className="sub-name" onChange={(e=>handleChange(e,i))}/>
+                </div>
+                <div>
+                  <input className="sub-weight" onChange={(e=>handleChange(e,i))}/>
+                </div>
+                <div>
+                  <input className="sub-point" onChange={(e=>handleChange(e,i))}/>
+                </div>
+                <div>
+                  <input className="sub-des" onChange={(e=>handleChange(e,i))}/>
+                </div>
+              </div>
+            )
+          })}
+        <div className="new-button">
+          <button onClick={()=>handleAddFour()}>Add</button>
+        </div>
+      </div>
+
     <h2>{numbers.number1}</h2>
     </>
   )
